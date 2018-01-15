932e4e9e71f35ef4519114d2f487590fe7f53da8
Joe Reckley <jdreckley@gmail.com>
2018-01-15 15:34:51 -0500

    updates dependencies, bumps to version 0.4.4

--- package.js
+++ package.js
@@ -1,41 +1,41 @@
 Package.describe({
   name: 'getstream:stream-meteor',
-  version: '0.4.3',
+  version: '0.4.4',
   summary: 'Getstream.io integration package for Meteor',
   git: 'https://github.com/GetStream/stream-meteor',
   documentation: 'README.md',
 });
 
 Npm.depends({
-  'getstream': '3.4.1',
-  'fibers': '1.0.8',
+  'getstream': '3.10.0',
+  'fibers': '2.0.0',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('ecmascript@0.1.6');
-  api.use(['cosmos:browserify@0.10.0'], 'client');
+  api.use('ecmascript');
+  api.use(['cosmos:browserify'], 'client');
   api.use(['underscore', 'mongo']);
-  api.use('matb33:collection-hooks@0.7.15');
-  api.use('dburles:mongo-collection-instances@0.3.4');
-  api.use('check@1.1.0');
+  api.use('matb33:collection-hooks');
+  api.use('dburles:mongo-collection-instances');
+  api.use('check');
 
   api.export('Stream');
   api.addFiles('src/namespace.js');
   api.addFiles('src/client.browserify.js', 'client');
   api.addFiles('src/server/namespace.js', 'server');
-  api.addFiles(['config/getstream.js', 
-                'stream-meteor.js',
-                'src/feed-manager.js',
-                'src/collections.js',
-                'src/activity.js',
-                'src/backend.js']);
+  api.addFiles(['config/getstream.js',
+    'stream-meteor.js',
+    'src/feed-manager.js',
+    'src/collections.js',
+    'src/activity.js',
+    'src/backend.js']);
   api.addFiles('src/server/publish.js', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
-  api.use('sanjo:jasmine@0.20.2');
+  api.use('sanjo:jasmine');
   api.use(['underscore', 'mongo']);
   api.use('insecure');
   api.use('accounts-base');
